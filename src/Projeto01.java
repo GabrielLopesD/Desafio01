26a6c5c2e9a401d184895ab84bc15fcbe86cddd2
GabrielLopesD <lopesgabrielof@gmail.com>
2020-07-20 17:17:52 -0300

    Adicionando novo arquivo

---
+++ src/Projeto01.java
@@ -0,0 +1,21 @@
+
+public class Projeto01 {
+/*Era uma vez a história de quatro homens: 
+ * João, José, Jacinto e Joel. 
+ * Os quatro eram construtores de barcos e em quatro dias conseguiam construir quatro embarcações. 
+ * Quanto tempo demoraria um dos quatro homens para construir um único barco?*/
+	
+	public static int pessoas = 4, dias = 4, resposta;
+
+	
+	public static void main(String[] args) {
+		descobrirResposta();
+	}
+	
+	public static void descobrirResposta() {
+		
+		resposta = pessoas/dias;
+		
+		System.out.println(resposta);
+	}
+}
